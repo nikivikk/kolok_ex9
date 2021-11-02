3d454ef7ae7132ee732c986feae4b3e01fc2cbf7
Vika <Vika@HOME-PC>
2021-11-03 01:30:02 +0300

    Добавьте файлы проекта.

---
+++ kolok_ex9/kolok_ex9.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main()
+{
+    int n;
+    std:: cin >> n;
+    int k = 1;
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 || i % 7 == 0) {
+            k *= i;
+        }
+    }
+    std:: cout << k;
+}
